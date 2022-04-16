a9c50a56cdc88f9a48b44f5555ed13464e7c9923
Nina Tobolkin <nina@socialsky.io>
2022-04-17 01:49:00 +0300

    bitcoin

---
+++ learn_go_pesah_2022/wallet/wallet.go
@@ -0,0 +1,14 @@
+package wallet
+
+type Bitcoin int
+type Wallet struct {
+	balance Bitcoin
+}
+
+func (w *Wallet) Deposit(amount Bitcoin) {
+	w.balance += amount
+}
+
+func (w *Wallet) Balance() Bitcoin {
+	return w.balance
+}
