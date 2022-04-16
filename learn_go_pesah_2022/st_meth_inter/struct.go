836b4e333dea4adaa34ad1730bb5a9dcd8dd489e
Nina Tobolkin <nina@socialsky.io>
2022-04-17 01:33:03 +0300

    struct, methods, interfaces

--- learn_go_pesah_2022/st_meth_inter/struct.go
+++ learn_go_pesah_2022/st_meth_inter/struct.go
@@ -1,5 +1,13 @@
 package st_meth_inter
 
+import "math"
+
+///////////////////////////----------------- Interface ------------------///////////////////////////////
+type Shape interface {
+	Area() float64
+}
+
+///////////////////////////----------------- Rectangle ------------------///////////////////////////////
 type Rectangle struct {
 	Width  float64
 	Height float64
@@ -9,6 +17,25 @@ func Perimeter(rectangle Rectangle) float64 {
 	return 2 * (rectangle.Width + rectangle.Height)
 }
 
-func Area(rectangle Rectangle) float64 {
-	return rectangle.Width * rectangle.Height
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+///////////////////////////----------------- Circle ------------------///////////////////////////////
+type Circle struct {
+	Radius float64
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
+}
+
+///////////////////////////----------------- Triangle ------------------///////////////////////////////
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+func (t Triangle) Area() float64 {
+	return (t.Base * t.Height) * 0.5
 }
